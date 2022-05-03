e9259b753b014689e85ddbd3300a716e26c7fad7
Ramil Kaharian <ramilkaharian25@gmail.com>
2022-05-03 22:17:02 +0800

    intial files

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import Home from './components/Home.vue';
+require('./bootstrap');
+
+
+createApp(Home).mount('#app')
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h1>Guidance Services System</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
