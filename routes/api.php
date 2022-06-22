74537e2774188a8e95971e429fcde7c2c8c27a48
Ramil Kaharian <ramilkaharian25@gmail.com>
2022-06-22 21:12:46 +0800

    controller for all api has been created

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CouncellingResultsController;
+use App\Http\Controllers\DownloadableFilesController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TestResultsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::resource('/users',UserController::class);
+Route::resource('/students',StudentController::class);
+Route::resource('/councelling_results',CouncellingResultsController::class);
+Route::resource('/downloadable_files',DownloadableFilesController::class);
+Route::resource('/test_results',TestResultsController::class);
